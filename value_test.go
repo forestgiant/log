48ae7673ebd7a5fabe472645c6fd5d9859863516
Stephan Baker <stephan@forestgiant.com>
2016-08-01 15:50:07 -0400

    Adding DefaultCaller ValueGenerator.

--- value_test.go
+++ value_test.go
@@ -1,6 +1,7 @@
 package log
 
 import (
+	"strings"
 	"testing"
 	"time"
 )
@@ -16,5 +17,35 @@ func TestDefaultTimestamp(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 		return
+	}
+}
+
+func TestCaller(t *testing.T) {
+	var tests = []struct {
+		vg               ValueGenerator
+		expectedFileName string
+	}{
+		{Caller(0), "value.go"},
+		{Caller(1), "value_test.go"},
+		{Caller(2), "testing.go"},
+	}
+
+	for _, test := range tests {
+		callerString, ok := test.vg().(string)
+		if !ok {
+			t.Error("Caller did not return a value of type string as expected.")
+			return
+		}
+
+		splits := strings.Split(callerString, ":")
+		if len(splits) < 2 {
+			t.Error("Caller did not return a value of the expected format.")
+			return
+		}
+
+		if splits[0] != test.expectedFileName {
+			t.Errorf("Caller did not return the expected file name.  Found %s but expected %s.\n", splits[0], test.expectedFileName)
+			return
+		}
 	}
 }
